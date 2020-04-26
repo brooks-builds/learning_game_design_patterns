16b87e05e1bddf1241b0b90ef1395fb2af1c897a
Brooks Patton <brooks@brookzerker.com>
2020-04-26 15:22:56 -0600

    can jump

--- game.js
+++ game.js
@@ -1,12 +1,24 @@
 function setup() {
     createCanvas(770, 700);
-    player = new Player(createVector(50, height - 25));
+    player = new Player(createVector(50, 25));
+    gravity = createVector(0, 1);
 }
 
 function draw() {
+    clear();
     fill(0);
     rect(0, height - 5, width, 5);
     player.render();
+
+    if (keyIsDown(KEYCODE_SPACE)) {
+        console.log('space pressed')
+        player.jump();
+    }
+    player.update();
+    player.applyForce(gravity);
+    player.hitGround(height - 5);
 }
 
 let player;
+const KEYCODE_SPACE = 32;
+let gravity;
--- player.js
+++ player.js
@@ -3,10 +3,39 @@ class Player {
         this.width = 5;
         this.height = 20;
         this.location = location;
+        this.velocity = createVector(0, 0);
+        this.accelecation = createVector(0, 0);
+        this.jumpForce = createVector(0, -10);
+        this.isJumping = false;
     }
 
     render() {
         fill(0);
         rect(this.location.x, this.location.y, this.width, this.height);
+    }
+
+    jump() {
+        if (!this.isJumping) {
+            this.applyForce(this.jumpForce);
+            this.isJumping = true;
+        }
+    }
+
+    applyForce(force) {
+        this.accelecation.add(force);
+    }
+
+    hitGround(groundLocation) {
+        if (this.location.y + this.height > groundLocation) {
+            this.location.y = groundLocation - this.height;
+            this.velocity.y = 0;
+            this.isJumping = false;
+        }
+    }
+
+    update() {
+        this.velocity.add(this.accelecation);
+        this.location.add(this.velocity);
+        this.accelecation.mult(0);
     }
 }
